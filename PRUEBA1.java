7079fd32974c41a7efaa5f4bfa24d6e914252d3d
Nacho <nribasbenosa@gmail.com>
2024-10-11 13:13:45 +0200

    1
    Ejercicio 1

--- PRUEBA1.java
+++ PRUEBA1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EJ1 {
+public class Prueba1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int lado=0;
