6edd8a6cbaa1d0f9a667130f7b58a452b22fa3e8
Nacho <nribasbenosa@gmail.com>
2024-10-11 13:10:19 +0200

    Ejercicio 1
    Ejercicio 1

---
+++ EJ1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EJ1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lado=0;
+        int area=0;
+        Scanner llegir = new Scanner(System.in);
+
+        lado = llegir.nextInt();
+
+        area = lado * lado;
+
+        System.out.println(area);
+
+        scanner.close();
+    }
+}
